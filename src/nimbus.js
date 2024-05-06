8ab8257a40e8ce6458a7625fb744695adbf25880
Matt Williams <thatjsguy555@gmail.com>
2024-05-05 22:57:05 -0600

    new files for library
    new files for library

---
+++ src/nimbus.js
@@ -0,0 +1,28 @@
+// Example of the consolidated Nimbus file
+import { reactive } from "vue";
+
+class Nimbus {
+  constructor(options) {
+    this.state = reactive(options.state || {});
+    this.actions = options.actions || {};
+    this.mutations = options.mutations || {};
+  }
+
+  dispatch(actionName, payload) {
+    if (this.actions[actionName]) {
+      this.actions[actionName](this, payload);
+    } else {
+      console.error(`Action "${actionName}" is not defined.`);
+    }
+  }
+
+  commit(mutationName, payload) {
+    if (this.mutations[mutationName]) {
+      this.mutations[mutationName](this.state, payload);
+    } else {
+      console.error(`Mutation "${mutationName}" is not defined.`);
+    }
+  }
+}
+
+export default Nimbus;
